f3e8c9f49a5b15900ff4568297dbf7af0162fbc7
sandipkumarjha <sandipkumarjha211@gmail.com>
2026-03-21 20:01:40 +0530

    Page1-done

--- script.js
+++ script.js
@@ -44,10 +44,16 @@ ease :"power4"
 })
 
 
-
-
 tl.to("#loader",{
 display:"none",
+})
+tl.from("#nav",{
+    opacity:0,
+})
+
+tl.from("#hero h1 , #hero2 h1,#hero3 h1,#hero4 h1",{
+    y:120,
+    stagger:0.2,
 })
 }
 loadingAnimation();
